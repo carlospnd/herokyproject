d87a0bda9ce28f512507d13704cbeca1230f83b7
carlospnd <cmd12398@gmail.com>
2021-06-23 19:11:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))  
-  .get('/', function(req, res){
-		var numero = init();
-		res.send(numero);
-	});
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express().use(express.static(path.join(__dirname, 'public')));
+express().get('/', function(req, res){
+	var numero = init();
+	res.send(numero);
+});
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 function init()
 {
