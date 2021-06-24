17b0c5a48367e0135bdae8af928712165d2a7454
carlospnd <cmd12398@gmail.com>
2021-06-23 19:22:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))  
-  .get('/', function(req, res){ var saludar = init(); res.send(numero); })
+  .get('/', function(req, res){ var saludar = init(); res.send(saludar); })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function init()
