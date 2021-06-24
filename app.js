b11bd8d84c18d5780e4bff270187aa9308d50116
carlospnd <cmd12398@gmail.com>
2021-06-23 18:51:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var id = req.query.id; // $_GET["id"]
+
 express()
   .use(express.static(path.join(__dirname, 'public')))  
-  .get('/', (req, res) => res.send('53'))
+  .get('/', function(req, res){
+  res.send('turno: ' + req.query.turno);
+});
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+
